e1c862e9e91379d8f316e37f3cd5dced75bcc3b3
leekeifuture <opnoe09@gmail.com>
2020-05-20 15:18:19 +0300

    Decomposed spread of props on separated props

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -10,7 +10,8 @@ class HeaderContainer extends React.Component {
 
     render() {
         return (
-            <Header {...this.props} />
+            <Header isAuth={this.props.isAuth}
+                    login={this.props.login} />
         )
     }
 }
